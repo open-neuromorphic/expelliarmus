f6f89078040f6ad74f872ca72ca75ac9bb70a763
Fabrizio Ottati <fabriziottati@gmail.com>
2022-11-10 16:07:23 +0100

    Added test for EVT3 and changed test samples names for consistency.

--- tests/test_file_decoding.py
+++ tests/test_file_decoding.py
@@ -1,12 +1,17 @@
 import expelliarmus
 
 def test_dat_decoding():
-    events = expelliarmus.read_dat('tests/sample-files/NCARS_obj_004397_td.dat')
+    events = expelliarmus.read_dat('tests/sample-files/dat_test_NCARS.dat')
 
     assert len(events) == 4407
 
 
 def test_evt2_decoding():
-    events = expelliarmus.read_evt2('tests/sample-files/sparklers.raw')
+    events = expelliarmus.read_evt2('tests/sample-files/evt2_test_sparklers.raw')
 
     assert len(events) == 521252
+
+def test_evt3_decoding():
+    events = expelliarmus.read_evt3('tests/sample-files/evt3_test_pedestrians.raw')
+
+    assert len(events) == 5000
