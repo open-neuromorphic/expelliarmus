02f8a15e2e05f5262ce23ce25bbe3e9549a600b5
Gregor Lenz <mail@lenzgregor.com>
2022-11-29 12:57:43 +0000

    bump version to 1.1.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     maintainer="Fabrizio Ottati, Gregor Lenz",
     maintainer_email="fabriziottati@gmail.com, mail@lenzgregor.com",
     url="https://github.com/fabhertz95/expelliarmus",
-    version="1.1.2",
+    version="1.1.3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=["expelliarmus", "expelliarmus.wizard"],
