38e208cb886eb17e8f0c78c16fcfc23385536b6a
fabrizio-ottati <fabriziottati@gmail.com>
2022-12-30 18:35:28 +0100

    Ported dat.c to 80 characters limit.

--- expelliarmus/src/dat.c
+++ expelliarmus/src/dat.c
@@ -4,20 +4,27 @@
 #include <stdint.h>
 #include <string.h>
 
-#define LOOP_CONDITION(window, last_t, ovfs, first_t) (window > (((ovfs << 32) | last_t) - first_t))
+#define LOOP_CONDITION(window, last_t, ovfs, first_t) (window > \
+        (((ovfs << 32) | last_t) - first_t))
 
-DLLEXPORT void measure_dat(const char* fpath, dat_cargo_t* cargo, size_t buff_size){
+DLLEXPORT void measure_dat( const char* fpath, 
+                            dat_cargo_t* cargo,
+                            size_t buff_size ){
 	FILE* fp = fopen(fpath, "rb"); 
 	MEAS_CHECK_FILE(fp, fpath, cargo); 
 	
 	// Jumping over the headers.
 	if (cargo->events_info.start_byte == 0){
-		MEAS_CHECK_JUMP_HEADER((cargo->events_info.start_byte = jump_header(fp, NULL, 0U)), cargo);
+		MEAS_CHECK_JUMP_HEADER( (cargo->events_info.start_byte = 
+                                    jump_header(fp, NULL, 0U)),
+                                cargo );
 		// Jumping two bytes.
 		MEAS_CHECK_FSEEK(fseek(fp, 2, SEEK_CUR), cargo); 
 		cargo->events_info.start_byte += 2; 
 	} else {
-		MEAS_CHECK_FSEEK(fseek(fp, (long)cargo->events_info.start_byte, SEEK_SET), cargo); 
+		MEAS_CHECK_FSEEK(fseek( fp, 
+                                (long)cargo->events_info.start_byte, SEEK_SET), 
+                                cargo ); 
 	}
 
 	// Buffer to read binary data.
@@ -38,18 +45,24 @@ DLLEXPORT void measure_dat(const char* fpath, dat_cargo_t* cargo, size_t buff_si
 	return;
 }
 
-DLLEXPORT void get_time_window_dat(const char* fpath, dat_cargo_t* cargo, size_t buff_size){
+DLLEXPORT void get_time_window_dat( const char* fpath, 
+                                    dat_cargo_t* cargo, 
+                                    size_t buff_size){
 	FILE* fp = fopen(fpath, "rb"); 
 	MEAS_CHECK_FILE(fp, fpath, cargo); 
 	
 	// Jumping over the headers.
 	if (cargo->events_info.start_byte == 0){
-		MEAS_CHECK_JUMP_HEADER((cargo->events_info.start_byte = jump_header(fp, NULL, 0U)), cargo);
+		MEAS_CHECK_JUMP_HEADER( (cargo->events_info.start_byte = 
+                                    jump_header(fp, NULL, 0U)), 
+                                cargo );
 		// Jumping two bytes.
 		MEAS_CHECK_FSEEK(fseek(fp, 2, SEEK_CUR), cargo); 
 		cargo->events_info.start_byte += 2; 
 	} else {
-		MEAS_CHECK_FSEEK(fseek(fp, (long)cargo->events_info.start_byte, SEEK_SET), cargo); 
+		MEAS_CHECK_FSEEK(fseek( fp, 
+                                (long)cargo->events_info.start_byte, SEEK_SET), 
+                                cargo); 
 	}
 
 	// Buffer to read binary data.
@@ -63,8 +76,12 @@ DLLEXPORT void get_time_window_dat(const char* fpath, dat_cargo_t* cargo, size_t
 	const uint64_t mask_32b=0xFFFFFFFFU;
 	
 	// Reading the file.
-	while (LOOP_CONDITION(time_window, last_t, time_ovfs, first_t) && (values_read = fread(buff, sizeof(*buff), buff_size, fp)) > 0){
-		for (j=0; LOOP_CONDITION(time_window, last_t, time_ovfs, first_t) && j < values_read; j++){
+	while ( LOOP_CONDITION(time_window, last_t, time_ovfs, first_t) && 
+            (values_read = fread(buff, sizeof(*buff), buff_size, fp)) > 0 ){
+		for (j=0;
+            LOOP_CONDITION(time_window, last_t, time_ovfs, first_t) && 
+                j < values_read; 
+            j++){
 			t = buff[j] & mask_32b; 
 			if (t < last_t)
 				time_ovfs++; 
@@ -84,12 +101,16 @@ DLLEXPORT void get_time_window_dat(const char* fpath, dat_cargo_t* cargo, size_t
 	return;
 }
 
-DLLEXPORT int read_dat(const char* fpath, event_t* arr, dat_cargo_t* cargo, size_t buff_size){
+DLLEXPORT int read_dat( const char* fpath, 
+                        event_t* arr, 
+                        dat_cargo_t* cargo, 
+                        size_t buff_size ){
 	FILE* fp = fopen(fpath, "rb"); 
 	CHECK_FILE(fp, fpath); 
 
 	if (cargo->events_info.start_byte == 0){
-		CHECK_JUMP_HEADER((cargo->events_info.start_byte = jump_header(fp, NULL, 0U))); 
+		CHECK_JUMP_HEADER(  (cargo->events_info.start_byte = 
+                                jump_header(fp, NULL, 0U))); 
 		CHECK_FSEEK(fseek(fp, 2, SEEK_CUR));
 		cargo->events_info.start_byte += 2;
 	} else {
@@ -109,7 +130,8 @@ DLLEXPORT int read_dat(const char* fpath, event_t* arr, dat_cargo_t* cargo, size
 	uint64_t lower=0, upper=0; 
 	
 	// Reading the file.
-	while (i < dim && (values_read = fread(buff, sizeof(*buff), buff_size, fp)) > 0){
+	while ( i < dim && 
+            (values_read = fread(buff, sizeof(*buff), buff_size, fp)) > 0 ){
 		for (j=0; i < dim && j < values_read; j++){
 			// Event timestamp.
 			lower = buff[j] & mask_32b; 
@@ -117,7 +139,9 @@ DLLEXPORT int read_dat(const char* fpath, event_t* arr, dat_cargo_t* cargo, size
 			if (lower < cargo->last_t) // Overflow.
 				cargo->time_ovfs++; 
 			timestamp = (timestamp_t)((cargo->time_ovfs<<32) | lower); 
-			CHECK_TIMESTAMP_MONOTONICITY(timestamp, ((cargo->time_ovfs << 32) | cargo->last_t));
+			CHECK_TIMESTAMP_MONOTONICITY(timestamp, 
+                                        ((cargo->time_ovfs << 32) | 
+                                            cargo->last_t));
 
 			arr[i].t = timestamp; 
 			cargo->last_t = lower; 
@@ -140,9 +164,17 @@ DLLEXPORT int read_dat(const char* fpath, event_t* arr, dat_cargo_t* cargo, size
 	return 0; 
 }	
 
-DLLEXPORT int save_dat(const char* fpath, event_t* arr, dat_cargo_t* cargo, size_t buff_size){
+DLLEXPORT int save_dat( const char* fpath, 
+                        event_t* arr, 
+                        dat_cargo_t* cargo, 
+                        size_t buff_size){
 	char header[200]; 
-	sprintf(header, "%c This DAT file has been generated through expelliarmus (https://github.com/fabhertz95/expelliarmus.git) %c%c Data file containing CD events %c%c Version 2 %c", (char)HEADER_START, (char)HEADER_END, (char)HEADER_START, (char)HEADER_END, (char)HEADER_START, (char)HEADER_END); 
+	sprintf(header, "%c This DAT file has been generated through expelliarmus \
+                    (https://github.com/fabhertz95/expelliarmus.git) %c%c \
+                    Data file containing CD events %c%c Version 2 %c",
+                    (char)HEADER_START, (char)HEADER_END, 
+                    (char)HEADER_START, (char)HEADER_END, 
+                    (char)HEADER_START, (char)HEADER_END); 
 	const size_t header_len = strlen(header); 
 	FILE* fp; 
 	if (cargo->events_info.start_byte == 0){
@@ -185,7 +217,10 @@ DLLEXPORT int save_dat(const char* fpath, event_t* arr, dat_cargo_t* cargo, size
 	return 0; 
 }
 
-DLLEXPORT size_t cut_dat(const char* fpath_in, const char* fpath_out, size_t new_duration, size_t buff_size){
+DLLEXPORT size_t cut_dat(const char* fpath_in, 
+                        const char* fpath_out, 
+                        size_t new_duration, 
+                        size_t buff_size){
 	FILE* fp_in = fopen(fpath_in, "rb"); 
 	CUT_CHECK_FILE(fp_in, fpath_in); 
 	FILE* fp_out = fopen(fpath_out, "wb"); 
@@ -207,8 +242,12 @@ DLLEXPORT size_t cut_dat(const char* fpath_in, const char* fpath_out, size_t new
 	uint64_t time_ovfs=0, timestamp=0, first_timestamp=0; 
 	// Converting duration from milliseconds to microseconds.
 	new_duration *= 1000; 
-	while ((timestamp-first_timestamp) < (uint64_t)new_duration && (values_read = fread(buff, sizeof(*buff), buff_size, fp_in)) > 0){
-		for (j=0; (timestamp-first_timestamp) < (uint64_t)new_duration && j<values_read; j+=2){
+	while ( (timestamp-first_timestamp) < (uint64_t)new_duration && 
+            (values_read = fread(buff, sizeof(*buff), buff_size, fp_in)) > 0 ){
+		for (j=0; 
+            (timestamp-first_timestamp) < (uint64_t)new_duration && 
+                j<values_read; 
+            j+=2){
 			// Event timestamp.
 			if (((uint64_t)buff[j]) < timestamp) // Overflow.
 				time_ovfs++; 
