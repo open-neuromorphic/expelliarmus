60a9dd36f63bc2ce669711ac5d6eb7550c72a98f
fabrizio-ottati <fabriziottati@gmail.com>
2023-01-02 23:03:25 +0100

    New version for conda-forge (thanks @Tobias-Fischer)

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     maintainer="Fabrizio Ottati, Gregor Lenz",
     maintainer_email="fabriziottati@gmail.com, mail@lenzgregor.com",
     url="https://github.com/open-neuromorphic/expelliarmus",
-    version="1.1.9",
+    version="1.1.10",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=[
